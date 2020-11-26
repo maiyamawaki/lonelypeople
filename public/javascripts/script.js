ee190ad6561e13b1b02e3dc20333fd2cabddf5a7
Mai Yamawaki <joooooy031006@icloud.com>
2020-11-27 00:05:56 +0900

    Scroll function added

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -5,10 +5,30 @@ document.addEventListener('DOMContentLoaded', () => {
 }, false);
 
 
+/*imgs*/
 function changeImg(){
   let num = Math.floor(Math.random()*(26-1) + 1)
   document.querySelector(".draws").src=`/img/${num}.png`
   console.log(num)
 }
+
+/*scroll*/
+ScrollReveal().reveal(".about", {
+  duration :1800,
+  scale : 2,
+  reset:true,
+}); 
+
+ScrollReveal().reveal(".imgDetail", {
+  duration :1800,
+  reset:true,
+})
+
+ScrollReveal().reveal(".about-detail",{
+  duration : 2000,
+  scale : 1.1,
+  reset:true,
+})
+
 
 
