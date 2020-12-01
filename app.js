3849a9386b83cc1b71be3f1238895e899d94d3c0
Mai Yamawaki <joooooy031006@icloud.com>
2020-12-02 00:32:08 +0900

    modified for deploy

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ mongoose
   });
 
 const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+// const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
 const app = express();
 
